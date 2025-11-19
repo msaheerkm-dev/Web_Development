a59b7229caf8abe2ec40249b53786bd6610819f2
Muhammed Saheer km <msaheerkm@gmail.com>
2025-11-19 10:45:17 +0530

    updated

--- EZBA/config/connection.js
+++ EZBA/config/connection.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const url = 'mongodb://127.0.0.1:27017';
+const url = '';
 const client = new MongoClient(url);
 
 let database;
@@ -8,7 +8,7 @@ let database;
 const connectDB = async () => {
     try {
         await client.connect();
-        database = client.db('Wattfeild'); // Replace with your DB name
+        database = client.db(''); // Replace with your DB name
         console.log('Connected to database');
     } catch (err) {
         console.error('Failed to connect to database:', err);
